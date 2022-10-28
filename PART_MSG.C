afe996009a5b0fda894cba013927acf8fcd8e571
Bernd Boeckmann <bernd-git@boeckmann.io>
2022-10-28 21:27:39 +0200

    change version to 2.37.99

--- PART_MSG.C
+++ PART_MSG.C
@@ -1,8 +1,8 @@
 #include "part.h"
 
      /* VERSION_DATE "2.37.05            June 10, 1998" */
-#define VERSION_DATE "2.99.00            Oct  27, 2022"
-#define VERSION      "2.99.00"
+#define VERSION_DATE "2.37.99            Oct  27, 2022"
+#define VERSION      "2.37.99"
 
 #define HOME_URL "https://codeberg.org/boeckmann/ranish"
 
