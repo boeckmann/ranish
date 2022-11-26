ab8fbefaf3e35601334467f88ada28661c95af42
Bernd Boeckmann <bernd-codeberg@boeckmann.io>
2022-11-26 16:51:35 +0100

    increase version to 2.90.03

--- PART_MSG.C
+++ PART_MSG.C
@@ -1,8 +1,8 @@
 #include "part.h"
 
      /* VERSION_DATE "2.37.05            June 10, 1998" */
-#define VERSION_DATE "2.90.02        built " __DATE__
-#define VERSION      "2.90.02"
+#define VERSION_DATE "2.90.03        built " __DATE__
+#define VERSION      "2.90.03"
 
 
 #ifdef LANG_de		/* German  */
