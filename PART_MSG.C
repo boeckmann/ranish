d5011eeeb5c63b8192c64b16c7f9ba8f6228315f
Bernd Boeckmann <bernd-codeberg@boeckmann.io>
2023-01-19 20:16:08 +0100

    prepare version 2.90.11

--- PART_MSG.C
+++ PART_MSG.C
@@ -1,8 +1,8 @@
 #include "part.h"
 
      /* VERSION_DATE "2.37.05            June 10, 1998" */
-#define VERSION_DATE "2.90.10        built " __DATE__
-#define VERSION      "2.90.10"
+#define VERSION_DATE "2.90.11        built " __DATE__
+#define VERSION      "2.90.11"
 
 
 #ifdef LANG_de		/* German  */
